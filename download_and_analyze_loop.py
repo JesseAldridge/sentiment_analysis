0d04362214181cf886708f35f03e60f62c75c53e
Jesse Aldridge <jessealdridge@gmail.com>
2020-06-16 11:11:22 -0700

    rewrite csv

--- download_and_analyze_loop.py
+++ download_and_analyze_loop.py
@@ -1,4 +1,4 @@
-import os, glob, time, pickle, csv
+import os, glob, time, pickle, csv, asyncio
 from datetime import datetime
 
 import twint
@@ -42,31 +42,32 @@ def main():
     if not os.path.exists(tweets_dir):
       os.makedirs(tweets_dir)
 
-    symbol_sentiments = []
-    for path in glob.glob(os.path.expanduser('~/stock_data/*.csv'))[:LIMIT]:
-      symbol = path.rsplit('.csv', 1)[0].rsplit('/', 1)[1]
+    timestamp = '_'.join(str(datetime.utcnow()).split()).rsplit('.', 1)[0].replace(':', '-')
+    sentiment_path = os.path.join(sentiment_dir, '{}'.format(timestamp)) + '.csv'
+    symbol_sentiment_rows = []
 
-      print('grabbing tweets for:', symbol)
-      tweets_path = download_tweets(symbol, tweets_dir)
+    for prices_path in glob.glob(os.path.expanduser('~/stock_data/*.csv'))[:LIMIT]:
+      symbol = prices_path.rsplit('.csv', 1)[0].rsplit('/', 1)[1]
+
+      print('{}, grabbing tweets for: {}'.format(datetime.utcnow(), symbol))
+      try:
+        tweets_path = download_tweets(symbol, tweets_dir)
+      except asyncio.exceptions.TimeoutError:
+        print('timeout error')
 
       with open(tweets_path) as f:
         tweet_rows = list(csv.DictReader(f))
       tweets = [Tweet(tweet_row.get('tweet')) for tweet_row in tweet_rows]
 
-      sum_ = 0
-      for tweet in tweets:
-        sum_ += tweet.sentiment
-      sentiment = sum_ / len(tweets)
-      symbol_sentiments.append((symbol, sentiment))
+      sentiment = sum(tweet.sentiment for tweet in tweets) / len(tweets)
+      symbol_sentiment_rows.append((symbol, sentiment))
+      symbol_sentiment_rows.sort(key=lambda t: t[-1])
 
-    symbol_sentiments.sort(key=lambda t: t[1])
-    timestamp = '_'.join(str(datetime.utcnow()).split()).rsplit('.', 1)[0].replace(':', '-')
-    path = os.path.join(sentiment_dir, '{}'.format(timestamp)) + '.csv'
-    with open(path, 'w') as f:
-      writer = csv.writer(f, lineterminator='\n')
-      writer.writerow(('symbol', 'sentiment'))
-      for row in symbol_sentiments:
-        writer.writerow(row)
+      with open(sentiment_path, 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(('symbol', 'sentiment'))
+        for row in symbol_sentiment_rows:
+          writer.writerow(row)
 
     print("{}, done, sleeping for 24 hours".format(datetime.utcnow()))
     time.sleep(60 * 60 * 24)
