9ccae4576b6d0191c990d99d2bdc4687dc807513
Jesse Aldridge <jessealdridge@gmail.com>
2020-12-29 22:14:24 -0600

    git rid of file handler

--- download_and_analyze_loop.py
+++ download_and_analyze_loop.py
@@ -10,15 +10,12 @@ def setup_logger():
   logger = logging.getLogger()
   logger.setLevel(logging.INFO)
 
-  fh = logging.FileHandler('my_log_info.log')
   sh = logging.StreamHandler(sys.stdout)
   formatter = logging.Formatter(
     '[%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s',
     datefmt='%a, %d %b %Y %H:%M:%S'
   )
-  fh.setFormatter(formatter)
   sh.setFormatter(formatter)
-  logger.addHandler(fh)
   logger.addHandler(sh)
 logger = setup_logger()
 
